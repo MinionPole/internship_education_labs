8629978c39d54ce6968ec1f91be790d9c730d61e
Roman <roma.yunusov.2001@mail.ru>
2024-08-13 19:08:13 +0300

    add waiting without double posedhe clk

--- block1/lab1_serializator/tb/testbench_2.sv
+++ block1/lab1_serializator/tb/testbench_2.sv
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module testbench_2;
+module testbench_2#(parameter MAX_WORK_TIMEOUT = 8, MAX_COOLDOWN_TIMEOUT = 100);
   
   bit clk;
   bit reset;
@@ -37,6 +37,7 @@ module testbench_2;
   logic how_many_data;
   int i;
 
+  int timeout_counter;
   
   serializator ser_obj(
       .clk_i(clk),
@@ -49,50 +50,80 @@ module testbench_2;
       .ser_data_o(ser_data_o),
       .ser_data_val_o(ser_data_val_o),
       .busy_o(busy_ota_o)
-      );
-      
+   );
+
+  task cooldown_wait();
+    begin
+      timeout_counter <= 0;
+      while( busy_ota_o && timeout_counter < MAX_COOLDOWN_TIMEOUT)
+        begin
+          @(posedge clk)
+          timeout_counter <= timeout_counter + 1;
+        end
+      if(timeout_counter == MAX_COOLDOWN_TIMEOUT)
+        begin
+         $error("too much busy");  
+        end    
+    end
+  endtask 
+
+  task insert_data(
+  input reset_t,
+  input         [15:0] data_i_t,
+  input         [3:0]  data_mod_i_t,
+  input                data_val_i_t);
+    begin
+      reset           <= reset_t;
+      data_i          <= data_i_t;
+      data_mod_i      <= data_mod_i_t;
+      data_valid      <= data_val_i_t;
+      how_many_data   <= (data_mod_i_t == 0) ? 4'd16 : data_mod_i_t;
+      timeout_counter <= 0;
+    end
+  endtask
+
+   
   task correct_data_check(
   input reset_t,
   input         [15:0] data_i_t,
   input         [3:0]  data_mod_i_t,
   input                data_val_i_t,
   
-  output  logic        res_flag_t
-  );
+  output  logic        res_flag_t);
     begin
-    
-     reset         <= reset_t;
-     data_i        <= data_i_t;
-     data_mod_i    <= data_mod_i_t;
-     data_valid    <= data_val_i_t;
-     res_flag_t    <= 1'b0;
-     how_many_data <= (data_mod_i_t == 0) ? 4'd16 : data_mod_i_t;
-     @(posedge clk)
-     @(posedge clk)
-     
+      insert_data(reset_t, data_i_t, data_mod_i_t, data_val_i_t);
+      res_flag_t    <= 1'b0;
+      while( !ser_data_val_o && timeout_counter <= MAX_WORK_TIMEOUT )
+        begin
+         @(posedge clk)
+         timeout_counter <= timeout_counter + 1;
+        end
+
+      
       for(i = 0;i < how_many_data;i = i + 1)
-      begin
+        begin
           @(posedge clk)
           if(ser_data_o != data_i[15-i])
-          begin
+            begin
               $error("error, wrong data out on first cycle in %d el, need = %b, get = %b", i, data_i[15-i], ser_data_val_o);
               res_flag_t <= 1'b1;
-          end
+            end
           
           if(busy_ota_o != 1)
-          begin
+            begin
               $error("error, must be busy on first cycle in %d el", i);
               res_flag_t <= 1'b1;
-          end
+            end
           
           if(ser_data_val_o != 1)
-          begin
+            begin
               $error("error, must be correct data_flag on first cycle in %d el", i);
               res_flag_t <= 1'b1;
-          end
-      end     
-     
-     data_valid <= 1'b0;
+            end
+          
+        end     
+      
+      data_valid <= 1'b0;
     end
   endtask
   
@@ -106,22 +137,23 @@ module testbench_2;
   output  logic        res_flag_t
   );
     begin    
-       reset         <= reset_t;
-       data_i        <= data_i_t;
-       data_mod_i    <= data_mod_i_t;
-       data_valid    <= data_val_i_t;
-       res_flag_t    <= 1'b0;
-       how_many_data <= (data_mod_i_t == 0) ? 4'd16 : data_mod_i_t;
-       @(posedge clk)
+      insert_data(reset_t, data_i_t, data_mod_i_t, data_val_i_t);
+      res_flag_t    <= 1'b0;
+
+      while( !busy_ota_o && timeout_counter <= MAX_WORK_TIMEOUT )
+        begin
+          @(posedge clk)
+          timeout_counter <= timeout_counter + 1;
+        end
        
-       if(busy_ota_o == 1)
-         begin
-           $error("error, mustn't be busy ");
-           res_flag_t <= 1'b1;
-         end
+      if(busy_ota_o == 1)
+        begin
+          $error("error, mustn't be busy ");
+          res_flag_t <= 1'b1;
+        end
          
-       data_valid <= 1'b0;
-     end
+      data_valid <= 1'b0;
+    end
   endtask
 
       
@@ -145,9 +177,8 @@ module testbench_2;
         $display("first data test ok");
      
      
-     
-     @(posedge clk)
-     @(posedge clk)
+
+
      $display("second data test start");
      correct_data_check(1'b0, 16'b1011000000000101, 4'd0, 1'b1, task_res_flag);
      if( task_res_flag )
@@ -155,9 +186,11 @@ module testbench_2;
      else
         $display("second data test ok");
         
-        
-     @(posedge clk)
-     @(posedge clk)
+    while( busy_ota_o)
+      begin
+        @(posedge clk)
+        timeout_counter <= timeout_counter + 1;
+      end
      $display("first wrong input test start");
      wrong_data_check_task(1'b0, 16'b1011000000000101, 4'd1, 1'b1, task_res_flag);
      if( task_res_flag )
@@ -165,8 +198,11 @@ module testbench_2;
      else
         $display("first wrong input test ok");
         
-     @(posedge clk)
-     @(posedge clk)
+    while( busy_ota_o)
+      begin
+        @(posedge clk)
+        timeout_counter <= timeout_counter + 1;
+      end
      $display("second wrong input test start");
      wrong_data_check_task(1'b0, 16'b1011000000000101, 4'd2, 1'b1, task_res_flag);
      if( task_res_flag )
