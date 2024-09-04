1e2c6228d37a8b10ed6a1c803b3463e926250d9d
Roman <roma.yunusov.2001@mail.ru>
2024-09-04 23:24:07 +0300

    two always blocks for every variable, add srst to data_val_o signal

--- block1/lab3_first_one/rtl/priority_encoder.sv
+++ block1/lab3_first_one/rtl/priority_encoder.sv
@@ -12,17 +12,22 @@ module priority_encoder
     output logic [(WIDTH-1):0]  data_right_o,
     output logic                data_val_o
   );
-  int right_o_ind;
+  
   int left_o_ind;
+  int right_o_ind;
   always_comb
     begin
       right_o_ind = -1;
-      left_o_ind = -1;
       for(int i = 0; i < WIDTH;i++)
         begin
           if(data_i[i] == 1)
             right_o_ind = i;
         end
+    end
+
+  always_comb
+    begin
+      left_o_ind = -1;
       for(int i = WIDTH - 1; i >= 0;i--)
         begin
           if(data_i[i] == 1)
@@ -66,7 +71,12 @@ module priority_encoder
 
   always_ff @(posedge clk_i)
     begin
-      data_val_o <= (data_val_i == 1);
+      if(srst_i)
+        begin
+          data_val_o <= 0;
+        end
+      else
+        data_val_o <= (data_val_i == 1);
     end
 
 endmodule
