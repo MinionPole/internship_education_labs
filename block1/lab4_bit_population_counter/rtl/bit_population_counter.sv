6d47760558a16ff796f8a99e6d6cf1cd843a4239
Roman <roma.yunusov.2001@mail.ru>
2024-09-13 18:32:58 +0300

    add registers dut

--- block1/lab4_bit_population_counter/rtl/bit_population_counter.sv
+++ block1/lab4_bit_population_counter/rtl/bit_population_counter.sv
@@ -13,37 +13,54 @@ module bit_population_counter #(
 
 localparam LOWLEVELSIZE = (WIDTH + SLICE - 1) / SLICE;
 
-logic [$clog2(LOWLEVELSIZE):0][LOWLEVELSIZE - 1:0][$clog2(WIDTH):0] cnt;
+logic [WIDTH-1:0][$clog2(WIDTH):0] cnt_low_level;
+logic [$clog2(LOWLEVELSIZE):0][WIDTH-1:0][$clog2(WIDTH):0] cnt;
+logic [$clog2(LOWLEVELSIZE):0][$clog2(LOWLEVELSIZE):0] level_sizes;
 
-
-int level_size;
-always_comb
+initial
   begin
-   level_size = LOWLEVELSIZE;
-	 for(int i = 0; i < LOWLEVELSIZE;i++)
-	  begin
-		 cnt[0][i] = '0;
-	    for(int j = 0; j < SLICE && i * SLICE + j < WIDTH;j++)
-		    cnt[0][i] += data_i[i * SLICE + j];
-     $display("i = %d val = %b", i, cnt[0][i]);
-		end
+    level_sizes[0] = LOWLEVELSIZE;
+    for(int top_level_ind = 1; top_level_ind <= $clog2(LOWLEVELSIZE);top_level_ind++)
+	    begin
+        level_sizes[top_level_ind] = (level_sizes[top_level_ind - 1] + 1) / 2;
+		    //$display("top_level_ind = %d, value is = %d", top_level_ind, level_sizes[top_level_ind]);
+		  end
+  end
 
-		 
+always_ff @( posedge clk_i )
+  begin
 	 for(int top_level_ind = 1; top_level_ind <= $clog2(LOWLEVELSIZE);top_level_ind++)
 	   begin
-		  for(int i = 0; i < (level_size + 1) / 2;i++)
+		  for(int i = 0; i < level_sizes[top_level_ind];i++)
 		    begin
-				  cnt[top_level_ind][i] = cnt[top_level_ind - 1][i * 2];
-          if(i * 2 + 1 < level_size)
-            cnt[top_level_ind][i] += cnt[top_level_ind - 1][i * 2 + 1];
-          $display("i = %d, j = %d, val = %b", top_level_ind, i, cnt[top_level_ind][i]);
-			  end
-      level_size = (level_size + 1) / 2;
+            if(i * 2 + 1 < level_sizes[top_level_ind - 1])
+              cnt[top_level_ind][i] <= cnt[top_level_ind - 1][i * 2 + 1] + cnt[top_level_ind - 1][i * 2];
+            else
+              cnt[top_level_ind][i] <= cnt[top_level_ind - 1][i * 2];
+			 end
+		end
+  end
+
+
+always_ff @( posedge clk_i )
+  begin
+    cnt[0] <= cnt_low_level;
+  end
+
+always_comb
+  begin
+    for(int i = 0; i < LOWLEVELSIZE;i++)
+	  begin
+		  cnt_low_level[i] = '0;
+	    for(int j = 0; j < SLICE && i * SLICE + j < WIDTH;j++)
+		    cnt_low_level[i] = cnt_low_level[i] + data_i[i * SLICE + j];
+      $display("i = %d val = %b", i, cnt_low_level[i]);
 		end
   end
 
 always_ff @( posedge clk_i )
   data_o <= cnt[$clog2(LOWLEVELSIZE)][0];
+
 
 always_ff @( posedge clk_i )
   if( srst_i )
--- block1/lab4_bit_population_counter/tb/make_sim.do
+++ block1/lab4_bit_population_counter/tb/make_sim.do
@@ -3,19 +3,7 @@ vlib work
 vlog -sv ../rtl/bit_population_counter.sv
 vlog -sv bit_population_counter_tb.sv
 
-for {set i 1} {$i <= 30} {incr i} {
-  vsim -novopt -gWIDTH=$i bit_population_counter_tb
-  add log -r /*
-  add wave /bit_population_counter_tb/clk
-  add wave /bit_population_counter_tb/srst
-  add wave /bit_population_counter_tb/data
-  add wave /bit_population_counter_tb/data_val_i
-  add wave /bit_population_counter_tb/data_o
-  add wave /bit_population_counter_tb/data_val_o
-  run -all
-  quit -sim
-}
-vsim -novopt -gWIDTH=50 bit_population_counter_tb
+vsim -novopt -gWIDTH=16 bit_population_counter_tb
 add log -r /*
 add wave /bit_population_counter_tb/clk
 add wave /bit_population_counter_tb/srst
@@ -24,15 +12,3 @@ add wave /bit_population_counter_tb/data_val_i
 add wave /bit_population_counter_tb/data_o
 add wave /bit_population_counter_tb/data_val_o
 run -all
-quit -sim
-
-vsim -novopt -gWIDTH=150 bit_population_counter_tb
-add log -r /*
-add wave /bit_population_counter_tb/clk
-add wave /bit_population_counter_tb/srst
-add wave /bit_population_counter_tb/data
-add wave /bit_population_counter_tb/data_val_i
-add wave /bit_population_counter_tb/data_o
-add wave /bit_population_counter_tb/data_val_o
-run -all
-quit -sim
